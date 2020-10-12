5ecfb0796a81dbca780c6c8dfcf904763d700ebb
Lungisani M Nhlengethwa <lm.nhlengethwa@outlook.com>
2020-10-12 11:11:50 +0200

    Add first batch of files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class RandomQuote extends Component {
+   constructor(props) {
+      super(props)
+      this.state = {
+         quote: '',
+         author: ''
+      }
+   }
+
+   componentDidMount() {
+      this.getQuote()
+   }
+
+   getQuote() {
+      let url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+
+      axios.get(url)
+         .then(res => {
+            let data = res.data.quotes
+            let quoteNum = Math.floor(Math.random() * data.length)
+            let randomQuote = data[quoteNum]
+
+            this.setState({
+               quote: randomQuote['quote'],
+               author: randomQuote['author']
+            })
+         })
+   }
+
+   getNewQuote = () => {
+      this.getQuote()
+   }
+
+   render() {
+      const { quote, author } = this.state
+      return (
+         <div id='wrapper'>
+            <h1 className='title'>Random Quotes</h1>
+
+            <div id='quote-box'>
+               <div id='text'><p>{quote}</p></div>
+               <div id='author'><h5>{author}</h5></div>
+
+               <div id='buttons'>
+                     <span>
+                        <i className="fab fa-twitter twitter-icon" />
+                     </span>
+                  <button id='new-quote' className='buttons' onClick={this.getNewQuote}>New Quote</button>
+               </div>
+            </div>
+         </div>
+      )
+   }
 }
 
-export default App;
+export default RandomQuote
---
+++ src/routes.js
@@ -0,0 +1,35 @@
+const { Router } = require('express')
+const listQuotes = require('./controllers/quotes/listQuotes')
+const getQuoteById = require('./controllers/quotes/getQuoteById')
+const searchQuotes = require('./controllers/search/searchQuotes')
+const randomQuote = require('./controllers/quotes/randomQuote')
+const listAuthors = require('./controllers/authors/listAuthors')
+const getAuthorById = require('./controllers/authors/getAuthorById')
+const listTags = require('./controllers/tags/listTags')
+
+const router = Router()
+
+/**------------------------------------------------
+ ** Quotes
+ **-----------------------------------------------*/
+router.get('/quotes', listQuotes)
+router.get('/quotes/:id', getQuoteById)
+router.get('/random', randomQuote)
+
+/**------------------------------------------------
+ ** Authors
+ **-----------------------------------------------*/
+router.get('/authors', listAuthors)
+router.get('/authors/:id', getAuthorById)
+
+/**------------------------------------------------
+ ** Tags
+ **-----------------------------------------------*/
+router.get('/tags', listTags)
+
+/**------------------------------------------------
+ ** Search
+ **-----------------------------------------------*/
+router.get('/search/quotes', searchQuotes)
+
+module.exports = router
